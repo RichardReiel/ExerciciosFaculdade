712036bd98fd3f6836ee4f823f879abeef14bbfb
RichardReiel <138708798+RichardReiel@users.noreply.github.com>
2025-05-27 23:20:38 -0300

    Add files via upload

---
+++ portfolio/portfolio/src/components/NavbarComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="bg-stone-950 text-white py-4 px-8 flex justify-between items-center">
+    <h1 class="text-lg font-bold">Richard Reiel Portfólio</h1>
+    <ul class="flex gap-6">
+      <li>
+        <router-link to="/" >Sobre Mim</router-link>
+      </li>
+      <li>
+        <router-link to="/trajetoria">Trajetória</router-link>
+      </li>
+      <li>
+        <router-link to="/portfolio">Portfólio</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ portfolio/portfolio/src/views/PortfolioView.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="p-6 md:p-12 max-w-6xl mx-auto">
+      <div class="mb-8">
+        <h1 class="text-4xl font-extrabold text-gray-800 mb-4">Meus Projetos</h1>
+        <p class="text-lg text-gray-600">Aqui estão alguns dos projetos que desenvolvi ao longo da minha graduação.</p>
+      </div>
+      
+      <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div class="bg-white rounded-xl shadow-lg p-6 hover:shadow-2xl transition">
+          <h2 class="text-2xl font-semibold text-gray-700 mb-2">Projeto 1</h2>
+          <p class="text-gray-600 mb-4">Sistema que traz informações de personagens da API do Dragon Ball Z. Tecnologias: HTML, CSS. JavaScript</p>
+          <a href="https://github.com/RichardReiel/ExerciciosFaculdade/tree/main/DBZ_API" class="text-blue-600 hover:underline font-medium">Ver mais →</a>
+        </div>
+  
+        <div class="bg-white rounded-xl shadow-lg p-6 hover:shadow-2xl transition">
+          <h2 class="text-2xl font-semibold text-gray-700 mb-2">Projeto 2</h2>
+          <p class="text-gray-600 mb-4">Lógica de um jogo de RPG com Classe e Objeto em PHP. Tecnologias: HTML, PHP.</p>
+          <a href="https://github.com/RichardReiel/ExerciciosFaculdade/tree/main/RPG" class="text-blue-600 hover:underline font-medium">Ver mais →</a>
+        </div>
+  
+        <div class="bg-white rounded-xl shadow-lg p-6 hover:shadow-2xl transition">
+          <h2 class="text-2xl font-semibold text-gray-700 mb-2">Projeto 3</h2>
+          <p class="text-gray-600 mb-4">Lógica de funcionamento de um escritório com armarios e gavetas. Tecnologias: HTML, PHP.</p>
+          <a href="https://github.com/RichardReiel/ExerciciosFaculdade/tree/main/Escritorio" class="text-blue-600 hover:underline font-medium">Ver mais →</a>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Projects'
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
